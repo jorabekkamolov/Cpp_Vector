d80d8d1834ab2fb448d8045254f1de94508eecf7
jorabek <turnerko>
2024-10-31 06:47:45 +0500

    push insert begin end

--- headers/vector_cpp.h
+++ headers/vector_cpp.h
@@ -23,11 +23,18 @@ public:
   Vector(std::initializer_list<value_type> list);
   Vector(size_type n);
   Vector(size_type n, value_type value);
+  Vector(Vector &);
   ~Vector();
 
   size_type size() const;
   size_type capacity() const;
   reference at(size_type i);
+  Vector &operator=(Vector &);
+  iterator begin();
+  iterator end();
+  void push_back(value_type);
+  void reserve(size_type);
+  iterator insert(iterator, value_type);
 };
 
 template <typename T>
@@ -59,6 +66,15 @@ template <typename T> Vector<T>::Vector(size_type n, value_type value) {
   }
 }
 
+template <typename T> inline Vector<T>::Vector(Vector &other) {
+  size_vector = other.size_vector;
+  capacity_vector = other.capacity_vector;
+  vector = new value_type[size_vector];
+  for (size_type i = 0; i < size_vector; i++) {
+    vector[i] = other.vector[i];
+  }
+}
+
 template <typename T> Vector<T>::~Vector() { delete[] vector; }
 
 template <typename T> typename Vector<T>::size_type Vector<T>::size() const {
@@ -72,6 +88,59 @@ typename Vector<T>::size_type Vector<T>::capacity() const {
 
 template <typename T> typename Vector<T>::reference Vector<T>::at(size_type i) {
   return vector[i];
+}
+
+template <typename T> inline Vector<T> &Vector<T>::operator=(Vector &other) {
+  if (this != &other) {
+    delete[] vector;
+
+    size_vector = other.size_vector;
+    capacity_vector = other.capacity_vector;
+    vector = new value_type[size_vector];
+    for (size_type i = 0; i < size_vector; i++) {
+      vector[i] = other.vector[i];
+    }
+  }
+  return *this;
+}
+
+template <typename T> typename Vector<T>::iterator Vector<T>::begin() {
+  return vector;
+}
+
+template <typename T> typename Vector<T>::iterator Vector<T>::end() {
+  return vector + (size_vector - 1);
+}
+
+template <typename T> void Vector<T>::push_back(value_type value) {
+  if (size_vector == capacity_vector) {
+    size_type new_capacity = (size_vector == 0) ? 1 : (capacity_vector * 2);
+    reserve(new_capacity);
+  }
+  vector[size_vector++] = value;
+}
+
+template <typename T> void Vector<T>::reserve(size_type new_capacity) {
+  iterator new_vector = new value_type[new_capacity];
+  for (size_type i = 0; i < size_vector; i++) {
+    new_vector[i] = vector[i];
+  }
+  delete[] vector;
+  vector = new_vector;
+  capacity_vector = new_capacity;
+}
+
+template <typename T>
+typename Vector<T>::iterator Vector<T>::insert(iterator pos, value_type value) {
+  if (size_vector == capacity_vector) {
+    size_type new_capacity = (size_vector == 0) ? 1 : (capacity_vector * 2);
+    reserve(new_capacity);
+  }
+  for (iterator it = this->end(); it != pos; it--) {
+    *(it + 1) = *it;
+  }
+  *pos = value;
+  return pos;
 }
 
 #endif
--- tests/test.cpp
+++ tests/test.cpp
@@ -7,7 +7,65 @@ TEST(Vector_test, Init1) {
 
 TEST(Vector_test, Init2) {
   Vector<int> v = {1, 2, 3};
+  EXPECT_EQ(v.at(2), 3);
   EXPECT_EQ(v.size(), 3);
+}
+
+TEST(Vector_test, Init3) {
+  Vector<int> v(10, 1);
+  EXPECT_EQ(v.at(2), 1);
+  EXPECT_EQ(v.size(), 10);
+  EXPECT_EQ(v.capacity(), 10);
+}
+
+TEST(Vector_test, copy1) {
+  Vector<int> v1 = {1, 2, 7, 5, 4};
+  Vector<int> v2(v1);
+  EXPECT_EQ(v1.size(), v2.size());
+  EXPECT_EQ(v1.at(2), v2.at(2));
+}
+
+TEST(Vector_test, copy2) {
+  Vector<int> v1 = {1, 2, 7, 5, 4};
+  Vector<int> v2;
+  v2 = v1;
+  EXPECT_EQ(v1.at(2), v2.at(2));
+}
+
+TEST(Vector_test, begin) {
+  Vector<int> v1 = {12, 2, 7, 5, 4};
+  auto it = v1.begin();
+  EXPECT_EQ(*it, v1.at(0));
+}
+
+TEST(Vector_test, end) {
+  Vector<int> v1 = {12, 2, 7, 5, 4};
+  auto it = v1.end();
+  EXPECT_EQ(*it, 4);
+}
+
+TEST(Vector_test, push) {
+  Vector<int> v1;
+  v1.push_back(12);
+  v1.push_back(13);
+  v1.push_back(14);
+  EXPECT_EQ(v1.at(0), 12);
+}
+
+TEST(Vector_test, insert) {
+  Vector<int> v1;
+  v1.push_back(12);
+  v1.push_back(13);
+  v1.push_back(14);
+  v1.push_back(15);
+  v1.push_back(16);
+  v1.push_back(17);
+  v1.push_back(18);
+  v1.push_back(19);
+  v1.push_back(20);
+  auto it = v1.insert((v1.begin() + 3), 22);
+  auto result = v1.begin() + 3;
+  EXPECT_EQ(*result, *it);
 }
 
 int main(int argc, char *argv[]) {
